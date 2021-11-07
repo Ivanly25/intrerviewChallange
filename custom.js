8a79ffc1f7a447c2045977890725d38a972eb36e
ivanly25 <kingivanly@gmail.com>
2021-11-07 16:30:03 -0600

    fixed h1

--- custom.js
+++ custom.js
@@ -12,10 +12,14 @@ $(() => {
 //   })
 // })
 
-var flip = 0
+// var flip = 0
 $('#modal').click(function () {
 	$('h1').toggle("slow")
 })
+$('#btn').click(function () {
+    $('h1').show()
+})
+
 })
 // $('#modal').onClick($('h1').css("display", "none"))
 // # button is called # modal
