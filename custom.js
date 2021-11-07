74e373463f9ef95858c9929ffb62df4179138166
ivanly25 <kingivanly@gmail.com>
2021-11-07 16:21:45 -0600

    h1 hiding

--- custom.js
+++ custom.js
@@ -1,7 +1,21 @@
 // if the modal button is clicked hide all h1 tags 
-// $('h1').hide()
-// $('h1').show()
-$('#modal').on('click', $('h1').show())
 
 
+$(() => {
+// $('.h1').show()
+//$('#modal').on('click', ('h1').hide())
+
+// $(document).ready(function(){
+//   $('h1').show()
+//   $('#modal').click(function(){
+//     $('h1').toggle();
+//   })
+// })
+
+var flip = 0
+$('#modal').click(function () {
+	$('h1').toggle("slow")
+})
+})
+// $('#modal').onClick($('h1').css("display", "none"))
 // # button is called # modal
